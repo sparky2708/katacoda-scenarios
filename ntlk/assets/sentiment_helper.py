cd6c77fe953b6fc0565808ccf388756c0d9015cb
Denis Abramov <daa82@columbia.edu>
2018-07-12 13:11:41 -0400

    Create sentiment_helper.py

---
+++ ntlk/assets/sentiment_helper.py
@@ -0,0 +1,9 @@
+def clean_all_words(all_words):
+    all_words_clean = []
+    stopwords_english = stopwords.words('english')
+    
+    for word in all_words:
+        if word not in stopwords_english and word not in string.punctuation:
+            all_words_clean.append(word)
+ 
+    return all_words_clean
