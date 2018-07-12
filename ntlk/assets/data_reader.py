bb67dffec9c20c7f024cd6662094625eb26ae687
Denis Abramov <daa82@columbia.edu>
2018-07-12 13:16:18 -0400

    Update data_reader.py

--- ntlk/assets/data_reader.py
+++ ntlk/assets/data_reader.py
@@ -1,4 +1,6 @@
 from nltk.corpus import movie_reviews
+from nltk.corpus import stopwords
+import string
 
 def read_reviews():
     documents = []
@@ -22,3 +24,13 @@ def get_all_words(reviews):
     all_words = []
     all_words = [word.lower() for word in movie_reviews.words()]
     return all_words
+
+def clean_all_words(all_words):
+    all_words_clean = []
+    stopwords_english = stopwords.words('english')
+    
+    for word in all_words:
+        if word not in stopwords_english and word not in string.punctuation:
+            all_words_clean.append(word)
+ 
+    return all_words_clean
