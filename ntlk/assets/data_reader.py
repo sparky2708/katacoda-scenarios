1595886f4d39ae57d029c36ee52da000ce22692b
Denis Abramov <daa82@columbia.edu>
2018-07-12 12:15:21 -0400

    Update data_reader.py

--- ntlk/assets/data_reader.py
+++ ntlk/assets/data_reader.py
@@ -7,9 +7,18 @@ def read_review_words():
  
     for category in movie_reviews.categories():
         for fileid in movie_reviews.fileids(category):
-            documents.append(movie_reviews.raw(file_id),movie_reviews.words(fileid), category))
+            documents.append(movie_reviews.words(fileid), category, movie_reviews.raw(file_id)))
 
     return documents
+
+def get_review_raw(doc):
+    print doc[2]
+    
+def get_review_sentiment(doc):
+    return doc[1]
+
+def get_review_words(doc):
+    return doc[0]
 
 def clean_all_words(all_words):
     all_words_clean = []
