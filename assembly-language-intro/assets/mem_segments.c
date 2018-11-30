733d8501409e5deec7fbce5d9e227a4d9b05df04
Denis Abramov <daa82@columbia.edu>
2018-11-30 17:37:56 -0500

    tt

--- assembly-language-intro/assets/mem_segments.c
+++ assembly-language-intro/assets/mem_segments.c
@@ -6,7 +6,7 @@ int global_init_var = 10;
 void someFunc() {
     int stack_var;
     //this variable is on the stack    
-    printf("function stack_var is at address: 0x%O8x\n", &stack_var);
+    printf("function stack_var is at address: 0x%08x\n", &stack_var);
 }
 
 int main() {
@@ -15,20 +15,20 @@ int main() {
     static int static_var;
     int *heap_var_ptr;
 
-    heap_var_ptr = (int *) malloc(sizeof(int))
+    heap_var_ptr = (int *) malloc(sizeof(int));
 
     //these variables are in the data segment
-    printf("global_init_var is at address: 0x%O8x\n", &global_init_var);
-    printf("static_init_var is at address: 0x%O8x\n", &static_init_var);
+    printf("global_init_var is at address: 0x%08x\n", &global_init_var);
+    printf("static_init_var is at address: 0x%08x\n", &static_init_var);
 
     //these variables are in the bss segment
-    printf("global_var is at address: 0x%O8x\n", &global_var);
-    printf("static_var is at address: 0x%O8x\n", &static_var);
+    printf("global_var is at address: 0x%08x\n", &global_var);
+    printf("static_var is at address: 0x%08x\n", &static_var);
 
     //this variable is in the heap segment
-    printf("heap_var is at address: 0x%O8x\n", &heap_var_ptr);
+    printf("heap_var is at address: 0x%08x\n", &heap_var_ptr);
 
     //these variables are in the stack segment
-    printf("stack_var is at address: 0x%O8x\n", &stack_var);
+    printf("stack_var is at address: 0x%08x\n", &stack_var);
     someFunc();
 }
